94512b5c4c953f6b63953d00da3e8a0e453f5d51
13471595439 <919878005qaz@163.com>
2022-06-23 02:43:11 +0800

    master commit2

--- src/main/java/com/powernode/git/GitTest.java
+++ src/main/java/com/powernode/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("你好叼毛");
         System.out.println("你好癫佬");
         System.out.println("你好嗨佬");
+        System.out.println("你好嗨佬test2");
     }
 }
