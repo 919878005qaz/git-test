6eb878a4cd555457774772c41e7b425d89f83a9e
13471595439 <919878005qaz@163.com>
2022-06-23 02:22:21 +0800

    third commit

--- src/main/java/com/powernode/git/GitTest.java
+++ src/main/java/com/powernode/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("你好靓仔");
         System.out.println("你好叼毛");
+        System.out.println("你好癫佬");
     }
 }
